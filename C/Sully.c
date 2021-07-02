6d96a7ab04dda2a7a819beba33c1204cbbc5d7c6
drozdov-v <youpaw@pop-os.localdomain>
2021-07-02 22:06:23 +0300

    Sully complete

--- C/Sully.c
+++ C/Sully.c
@@ -1,13 +1,15 @@
 #include <stdio.h>
-#define N_LINES 43
-#define SRC_LINE 7
-#define CNT_LINE 23
-char	g_src_code[N_LINES][51] = {
+#include <stdlib.h>
+#define N_LINES 47
+#define SRC_LINE 8
+#define CNT_LINE 24
+char	g_src_code[N_LINES][66] = {
 		{"#include <stdio.h>"},
-		{"#define N_LINES 43"},
-		{"#define SRC_LINE 7"},
+		{"#include <stdlib.h>"},
+		{"#define N_LINES 47"},
+		{"#define SRC_LINE 8"},
 		{"#define CNT_LINE 23"},
-		{"char	g_src_code[N_LINES][51] = {"},
+		{"char	g_src_code[N_LINES][66] = {"},
 		{"};"},
 		{""},
 		{"static void	fprint_esc(FILE *f, char *s)"},
@@ -24,9 +26,9 @@ char	g_src_code[N_LINES][51] = {
 		{"	}"},
 		{"}"},
 		{""},
-		{"int	main(void)"},
+		{"int	main(int argc, char **argv, char **env)"},
 		{"{"},
-		{""},
+		{"	int i = 5;"},
 		{"	FILE *f = fopen(\"Sully_\"i\".c\", \"w\");"},
 		{"	if (!f)"},
 		{"		return(1);"},
@@ -34,7 +36,7 @@ char	g_src_code[N_LINES][51] = {
 		{"		fprintf(f, \"%s\\n\", g_src_code[l]);"},
 		{"	for (int l=0; l<N_LINES; l++)"},
 		{"	{"},
-		{"		fprintf(\"\t{\\\"\");"},
+		{"		fprintf(\"	{\\\"\");"},
 		{"		fprint_esc(g_src_code[l]);"},
 		{"		fprintf(\"\\\"},\\n\");"},
 		{"	}"},
@@ -45,6 +47,9 @@ char	g_src_code[N_LINES][51] = {
 		{"		else"},
 		{"			fprintf(f, \"%s\\n\", g_src_code[l]);"},
 		{"	}"},
+		{"	if (i >= 0)"},
+		{"		return (system(\"gcc -o Sully_\"i\" Sully_\"i\".c;./Sully_\"i));"},
+		{"	return (0);"},
 		{"}"},
 };
 
@@ -62,7 +67,7 @@ static void	fprint_esc(FILE *f, char *s)
 	}
 }
 
-int	main(void)
+int	main(int argc, char **argv, char **env)
 {
 	int i = 5;
 	FILE *f = fopen("Sully_"i".c", "w");
@@ -79,8 +84,11 @@ int	main(void)
 	for (int l=SRC_LINE; l<N_LINES; l++)
 	{
 		if (l == SRC_LINE)
-			fprintf(f, "	int i = "i";\n")
+			fprintf(f, "	int i = "i";\n");
 		else
 			fprintf(f, "%s\n", g_src_code[l]);
 	}
+	if (i >= 0)
+		return (system("clang -o Sully_"i" Sully_"i".c;./Sully_"i));
+	return (0);
 }
