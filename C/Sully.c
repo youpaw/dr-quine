8a807e7a2a209bd45d8ad8dbb91162e4abe1c09a
drozdov-v <youpaw@pop-os.localdomain>
2021-07-03 20:07:16 +0300

    Fix Sully

--- C/Sully.c
+++ C/Sully.c
@@ -1,15 +1,15 @@
 #include <stdio.h>
 #include <stdlib.h>
-#define N_LINES 47
+#define N_LINES 53
 #define SRC_LINE 8
 #define CNT_LINE 24
-char	g_src_code[N_LINES][66] = {
+char	g_src_code[N_LINES][80] = {
 		{"#include <stdio.h>"},
 		{"#include <stdlib.h>"},
-		{"#define N_LINES 47"},
+		{"#define N_LINES 53"},
 		{"#define SRC_LINE 8"},
 		{"#define CNT_LINE 23"},
-		{"char	g_src_code[N_LINES][66] = {"},
+		{"char	g_src_code[N_LINES][80] = {"},
 		{"};"},
 		{""},
 		{"static void	fprint_esc(FILE *f, char *s)"},
@@ -29,7 +29,12 @@ char	g_src_code[N_LINES][66] = {
 		{"int	main(int argc, char **argv, char **env)"},
 		{"{"},
 		{"	int i = 5;"},
-		{"	FILE *f = fopen(\"Sully_\"i\".c\", \"w\");"},
+		{"	char filename[20];"},
+		{"	char binname[20];"},
+		{"	char cmdname[60];"},
+		{"	sprintf(filename, \"Sully_%d.c\", i);"},
+		{"	sprintf(binname, \"Sully_%d\", i);"},
+		{"	FILE *f = fopen(filename);"},
 		{"	if (!f)"},
 		{"		return(1);"},
 		{"	for (int l=0; l<SRC_LINE; l++)"},
@@ -47,8 +52,9 @@ char	g_src_code[N_LINES][66] = {
 		{"		else"},
 		{"			fprintf(f, \"%s\\n\", g_src_code[l]);"},
 		{"	}"},
+		{"	sprintf(cmdname, \"clang -o %s %s;./%s\", binname, filename, binname);"},
 		{"	if (i >= 0)"},
-		{"		return (system(\"gcc -o Sully_\"i\" Sully_\"i\".c;./Sully_\"i));"},
+		{"		return (system(cmdname));"},
 		{"	return (0);"},
 		{"}"},
 };
@@ -70,16 +76,21 @@ static void	fprint_esc(FILE *f, char *s)
 int	main(int argc, char **argv, char **env)
 {
 	int i = 5;
-	FILE *f = fopen("Sully_"i".c", "w");
+	char filename[20];
+	char binname[20];
+	char cmdname[60];
+	sprintf(filename, "Sully_%d.c", i);
+	sprintf(binname, "Sully_%d", i);
+	FILE *f = fopen(filename, "w");
 	if (!f)
 		return (1);
 	for (int l=0; l<SRC_LINE; l++)
 		fprintf(f, "%s\n", g_src_code[l]);
 	for (int l=0; l<N_LINES; l++)
 	{
-		fprintf("	{\"");
+		fprintf(f, "	{\"");
 		fprint_esc(g_src_code[l]);
-		fprintf("\"},\n");
+		fprintf(f, "\"},\n");
 	}
 	for (int l=SRC_LINE; l<N_LINES; l++)
 	{
@@ -88,7 +99,8 @@ int	main(int argc, char **argv, char **env)
 		else
 			fprintf(f, "%s\n", g_src_code[l]);
 	}
+	sprintf(cmdname, "clang -o %s %s;./%s", binname, filename, binname);
 	if (i >= 0)
-		return (system("clang -o Sully_"i" Sully_"i".c;./Sully_"i));
+		return (system(cmdname));
 	return (0);
 }
