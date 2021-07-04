7f72fe950bb0ba4c4bd7849a7f799d2fe9bba5d0
drozdov-v <youpaw@pop-os.localdomain>
2021-07-04 19:03:45 +0300

    Complete Sully

--- C/Sully.c
+++ C/Sully.c
@@ -1,14 +1,14 @@
 #include <stdio.h>
 #include <stdlib.h>
-#define N_LINES 53
-#define SRC_LINE 8
+#define N_LINES 55
+#define SRC_LINE 6
 #define CNT_LINE 24
 char	g_src_code[N_LINES][80] = {
 		{"#include <stdio.h>"},
 		{"#include <stdlib.h>"},
-		{"#define N_LINES 53"},
-		{"#define SRC_LINE 8"},
-		{"#define CNT_LINE 23"},
+		{"#define N_LINES 55"},
+		{"#define SRC_LINE 6"},
+		{"#define CNT_LINE 24"},
 		{"char	g_src_code[N_LINES][80] = {"},
 		{"};"},
 		{""},
@@ -26,34 +26,36 @@ char	g_src_code[N_LINES][80] = {
 		{"	}"},
 		{"}"},
 		{""},
-		{"int	main(int argc, char **argv, char **env)"},
+		{"int	main(void)"},
 		{"{"},
 		{"	int i = 5;"},
 		{"	char filename[20];"},
 		{"	char binname[20];"},
 		{"	char cmdname[60];"},
+		{"	i--;"},
 		{"	sprintf(filename, \"Sully_%d.c\", i);"},
 		{"	sprintf(binname, \"Sully_%d\", i);"},
-		{"	FILE *f = fopen(filename);"},
+		{"	FILE *f = fopen(filename, \"w\");"},
 		{"	if (!f)"},
-		{"		return(1);"},
+		{"		return (1);"},
 		{"	for (int l=0; l<SRC_LINE; l++)"},
 		{"		fprintf(f, \"%s\\n\", g_src_code[l]);"},
 		{"	for (int l=0; l<N_LINES; l++)"},
 		{"	{"},
-		{"		fprintf(\"	{\\\"\");"},
-		{"		fprint_esc(g_src_code[l]);"},
-		{"		fprintf(\"\\\"},\\n\");"},
+		{"		fprintf(f, \"		{\\\"\");"},
+		{"		fprint_esc(f, g_src_code[l]);"},
+		{"		fprintf(f, \"\\\"},\\n\");"},
 		{"	}"},
 		{"	for (int l=SRC_LINE; l<N_LINES; l++)"},
 		{"	{"},
-		{"		if (l == SRC_LINE)"},
-		{"			fprintf(f, \"\tint i = \"i\";\\n\")"},
+		{"		if (l == CNT_LINE)"},
+		{"			fprintf(f, \"	int i = %d;\\n\", i);"},
 		{"		else"},
 		{"			fprintf(f, \"%s\\n\", g_src_code[l]);"},
 		{"	}"},
-		{"	sprintf(cmdname, \"clang -o %s %s;./%s\", binname, filename, binname);"},
-		{"	if (i >= 0)"},
+		{"	fclose(f);"},
+		{"	sprintf(cmdname, \"clang -o %s %s && ./%s\", binname, filename, binname);"},
+		{"	if (i > 0)"},
 		{"		return (system(cmdname));"},
 		{"	return (0);"},
 		{"}"},
@@ -73,12 +75,13 @@ static void	fprint_esc(FILE *f, char *s)
 	}
 }
 
-int	main(int argc, char **argv, char **env)
+int	main(void)
 {
 	int i = 5;
 	char filename[20];
 	char binname[20];
 	char cmdname[60];
+	i--;
 	sprintf(filename, "Sully_%d.c", i);
 	sprintf(binname, "Sully_%d", i);
 	FILE *f = fopen(filename, "w");
@@ -88,19 +91,20 @@ int	main(int argc, char **argv, char **env)
 		fprintf(f, "%s\n", g_src_code[l]);
 	for (int l=0; l<N_LINES; l++)
 	{
-		fprintf(f, "	{\"");
-		fprint_esc(g_src_code[l]);
+		fprintf(f, "		{\"");
+		fprint_esc(f, g_src_code[l]);
 		fprintf(f, "\"},\n");
 	}
 	for (int l=SRC_LINE; l<N_LINES; l++)
 	{
-		if (l == SRC_LINE)
-			fprintf(f, "	int i = "i";\n");
+		if (l == CNT_LINE)
+			fprintf(f, "	int i = %d;\n", i);
 		else
 			fprintf(f, "%s\n", g_src_code[l]);
 	}
-	sprintf(cmdname, "clang -o %s %s;./%s", binname, filename, binname);
-	if (i >= 0)
+	fclose(f);
+	sprintf(cmdname, "clang -o %s %s && ./%s", binname, filename, binname);
+	if (i > 0)
 		return (system(cmdname));
 	return (0);
 }
